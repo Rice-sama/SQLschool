76a045043c36fbb83c4f92a8e9f3921e53f460ff
Philippe Su <philippe_su@hotmail.fr>
2021-03-28 22:06:53 +0200

    fix conflict

--- SQLschool/src/apprendreSQL/view/version2/Gui.java
+++ SQLschool/src/apprendreSQL/view/version2/Gui.java
@@ -1,14 +1,7 @@
 package apprendreSQL.view.version2;
 
-<<<<<<< HEAD
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Path;
-
-import apprendreSQL.Controller.version1.EventManager;
 import apprendreSQL.view.version2.fileXml.Config;
-=======
->>>>>>> 94403e61c9b85c6732ccac968761e4a780b14e58
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,18 +14,12 @@ public class Gui extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-<<<<<<< HEAD
-		
-		Parent parent = FXMLLoader.load(getClass().getResource(Config.getHomaPage()));		
-		Scene scene = new Scene(parent,Config.WIDTH, Config.HEIGHT);
-=======
-		FXMLLoader fxmlloader = new FXMLLoader();
-		Parent parent = fxmlloader.load(getClass().getResource("fileXml/gui.fxml").openStream());
-		Scene scene = new Scene(parent,1280,800);
->>>>>>> 94403e61c9b85c6732ccac968761e4a780b14e58
-		primaryStage.setScene(scene);
-		primaryStage.setTitle(title);
-		primaryStage.show();
+
+        Parent parent = FXMLLoader.load(getClass().getResource(Config.getHomaPage()));       
+        Scene scene = new Scene(parent,Config.WIDTH, Config.HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(title);
+        primaryStage.show();
 	}
 	
 	
